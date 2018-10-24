ea1bb9780a589c06f22121f5e4368edb5a95bb63
Gaurav <netgary7@gmail.com>
2018-10-24 04:17:37 +0000

    adding oop-3.js

---
+++ oop-3.js
@@ -0,0 +1,48 @@
+
+//Person is the superclass of Student class and mentor class 
+class Person {
+  constructor(name, quirkyFact, email) {
+    this.name = name;
+    this.quirkyFact = quirkyFact;
+    this.email = email;
+    }
+
+  bio() {
+    return `My name is ${this.name}, my email is ${this.email} here's my quirky fact :${this.quirkyFact}`;
+  }
+}
+
+class Student extends Person {
+  enroll(cohort) {
+   this.cohort = cohort; 
+  }
+  bio() {
+    return `I am a student at Lighthouse labs. ${super.bio()}`;
+    // SUPER - method lets you call the bio() of super class within the bio() of student class.
+  }
+}
+
+class Mentor extends Person {
+  goOnShift() {
+    this.onShift = true;
+  }
+  goOffShift() {
+    this.onShift = false;
+  }
+}
+// let newPerson = new Person('Gaurav', 'Loves to fly', 'gaurav@celltrader.ca')
+// console.log(newPerson);
+// console.log(newPerson.bio());
+
+let newStudent = new Student('Gaurav', 'Loves to travel', 'gaurav@celltrader.ca');
+console.log(newStudent);
+newStudent.enroll('sept24');
+console.log(newStudent.cohort);
+console.log(newStudent.bio());
+
+// let newMentor = new Mentor('Sam', 'english accent', 'sam@sam.ca')
+// console.log(newMentor);
+// console.log(newMentor.bio());
+// newMentor.goOffShift();
+// console.log(newMentor.onShift);
+// // newMentor.onShift;
